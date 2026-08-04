dfdd3dac79d8970142d1ac0f7974b1b81ff4e7b8
Attack Master <attackmaster6663@gmail.com>
2026-08-04 19:49:57 +0530

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,23 @@
+// Chanakya Coaching Centre - MCQ Database
+const quizData = [
+    {
+        question: "द्विघात समीकरण x² − 5x + 6 = 0 के मूल हैं:",
+        options: ["1 और 6", "2 और 3", "3 और 5", "1 और 5"],
+        answer: 1 // (B) 2 और 3 (प्रोग्रामिंग में गिनती 0 से शुरू होती है, इसलिए B = 1)
+    },
+    {
+        question: "√144 = ?",
+        options: ["10", "11", "12", "13"],
+        answer: 2 // (C) 12
+    },
+    {
+        question: "15² = ?",
+        options: ["200", "210", "225", "250"],
+        answer: 2 // (C) 225
+    },
+    {
+        question: "यदि 2x + 3 = 15, तो x = ?",
+        options: ["5", "6", "7", "8"],
+        answer: 1 // (B) 6
+    }
+];
